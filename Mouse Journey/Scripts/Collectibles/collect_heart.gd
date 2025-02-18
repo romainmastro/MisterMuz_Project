a75518be56a48def8c255e13370608943b8ab91e
romainmastro <romain.mastro@gmail.com>
2025-02-18 21:58:54 +0100

    Collectible : Hovering

--- Mouse Journey/Scripts/Collectibles/collect_heart.gd
+++ Mouse Journey/Scripts/Collectibles/collect_heart.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
+@export var hoveringObjectComponent : ClassHoveringObjectComponent
+
 # if current_HP < max_HP : gains 1 HP
 func _ready() -> void:
 	scale = Vector2(1, 1)
-	#$Sprite2D.scale = Vector2(0.5, 0.5)
-	
+	hoveringObjectComponent.float_up()
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerClass : 
 		if GlobalPlayerStats.player_current_HP >= GlobalPlayerStats.player_max_HP : 
@@ -13,3 +15,6 @@ func _on_body_entered(body: Node2D) -> void:
 		else : 
 			GlobalPlayerStats.player_current_HP += 1
 			call_deferred("queue_free")
+
+
+	
