0c6739d9010c3eba728e2ba4a7614a3d794f10cd
romainmastro <romain.mastro@gmail.com>
2025-03-08 22:37:57 +0100

    Snowball + End level Asset
    - Modif: Snowball can be edited
    - Added : Asset for end of Level (Sign with flowers)
    - Modif : Stalagtite tombantes can be edited

--- Mouse Journey/Scripts/Traps/snowball.gd
+++ Mouse Journey/Scripts/Traps/snowball.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 
 @export var damage_amount : float = 1
 
+
 # disappear after 5 secs
 func _on_timer_timeout() -> void:
 	call_deferred("queue_free")
--- Mouse Journey/Scripts/Traps/snowball_spawner.gd
+++ Mouse Journey/Scripts/Traps/snowball_spawner.gd
@@ -4,16 +4,27 @@ extends Node2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var timer: Timer = $Timer
 
+@export var roule_droite : bool = true
+@export_range(3.0, 20.0) var vitesse := 7.0
+var roule_direction : int = 1
+
 @export var temps_apparition : float = 2.0
+
+var sb : TrapSnowballClass
 
 func _ready() -> void:
 	timer.wait_time = temps_apparition
 	sprite_2d.hide()
+	
+	roule_direction = 1 if roule_droite == true else -1
 
 func spawner() : 
-	var sb = snowball.instantiate()
+	sb = snowball.instantiate()
 	add_child(sb, true)
-
-
+	
 func _on_timer_timeout() -> void:
 	spawner()
+
+func _physics_process(_delta: float) -> void:
+	if sb : 
+		sb.apply_torque_impulse(vitesse * roule_direction)
