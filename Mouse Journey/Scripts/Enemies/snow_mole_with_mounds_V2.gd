f8681a82b486eae4f6abdb00794567b6f2448e94
romainmastro <romain.mastro@gmail.com>
2025-05-09 16:51:58 +0200

    Update SnowMole enemy

--- Mouse Journey/Scripts/Enemies/snow_mole_with_mounds_V2.gd
+++ Mouse Journey/Scripts/Enemies/snow_mole_with_mounds_V2.gd
@@ -9,18 +9,26 @@ var current_mound_index : int = 0
 @export var attacking_cooldown : Timer
 @export var ray_right : RayCast2D
 @export var ray_left : RayCast2D
-
-@onready var debug_state: Label = $snow_mole/Control/Label
-
+@export var collision_shape : CollisionShape2D
+@export var hitbox : Area2D
+@export var deadzone : Area2D
 
 @export_enum("IDLING", "SWITCHING_MOUND","TRAVELING_UNDERGROUND", "ATTACKING", "DEAD") var current_state = "IDLING"
 @export var gravity : float = 600.0
 @export var hop_force : float = -80.0
 @export var attacking_force : float = -200.0
+@export var switch_mound_wait_time : float = 1
+@export var underground_timer_wait_time : float = 1
+@export var death_pop_up_offset : float = -12.0
+
 var hop_initiated : bool = false
 var has_left_floor : bool = false
 var has_attacked_once : bool = false
 var has_left_floor_to_attack : bool = false
+var snow_mole_collision_enabled : bool = true
+
+
+var is_dead : bool = false
 
 var snow_mole_scale : Vector2 = Vector2(0.8, 0.8)
 
@@ -28,9 +36,14 @@ func _ready() -> void:
 	start_idle()
 	snow_mole.global_position = MOUNDS[0].global_position 
 	snow_mole.scale = snow_mole_scale
+	
+	switch_mound_wait_time = randf_range(0.8, 1.8)
+	switch_mound_timer.wait_time = switch_mound_wait_time
+	
+	underground_timer_wait_time = randf_range(0.6, 1.2)
+	underground_timer.wait_time = underground_timer_wait_time
 
 func _physics_process(delta: float) -> void:
-	
 	apply_gravity(delta)
 	
 	flip_snow_mole()
@@ -40,7 +53,8 @@ func _physics_process(delta: float) -> void:
 		"IDLING" : 
 			hop_initiated = false
 			has_left_floor = false
-			
+			if not snow_mole_collision_enabled : 
+				enable_snow_mole_collision()
 			
 			#animation
 			start_idle()
@@ -71,7 +85,8 @@ func _physics_process(delta: float) -> void:
 				
 		"TRAVELING_UNDERGROUND" : 
 			# Do nothing : just wait for underground_timer to timeout
-			pass
+			if snow_mole_collision_enabled : 
+				disable_snow_mole_collision()
 		
 		"ATTACKING" : 
 			if not has_attacked_once : 
@@ -85,14 +100,14 @@ func _physics_process(delta: float) -> void:
 			if has_left_floor_to_attack and snow_mole.is_on_floor() : 
 				attacking_cooldown.start()
 				current_state = "IDLING"
-	
-	
-	
-	snow_mole.move_and_slide()
-	
-	
-	get_debug_state()
-	
+		
+		"DEAD" : 
+			print("State : Dead")
+			handle_death()
+			
+	if not is_dead : 
+		snow_mole.move_and_slide()
+
 func apply_gravity(delta : float) : 
 	snow_mole.velocity.y += gravity * delta
 
@@ -127,6 +142,65 @@ func _on_attacking_cooldown_timeout() -> void:
 	has_attacked_once = false
 	has_left_floor_to_attack = false
 
-######################"" DEBUG ############################
-func get_debug_state() : 
-	debug_state.text = current_state
+func handle_death():
+	if is_dead : 
+		return
+	is_dead = true
+	
+	await do_hit_stop()
+	
+	if snow_mole_collision_enabled : 
+		disable_snow_mole_collision()
+
+	## flash Red
+	# Slight pop up before death animation
+	var tween = create_tween()
+	var start_pos = snow_mole.position
+	var end_pos = start_pos + Vector2(0, death_pop_up_offset)
+
+	tween.tween_property(snow_mole, "position", end_pos, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+	# After moving up, flash + play death animation
+	tween.chain().tween_callback(func ():
+		var flash = create_tween()
+		flash.tween_property(self, "modulate", Color.AQUA, 0.1)
+		flash.chain().tween_property(self, "modulate", Color.TRANSPARENT, 0.1)
+		flash.chain().tween_property(self, "modulate", Color.WHITE, 0.1)
+		snow_mole_anim_sprite.play("death")
+	)
+	
+	snow_mole_anim_sprite.play("death")
+
+func disable_snow_mole_collision() : 
+	snow_mole_collision_enabled = false
+	
+	collision_shape.set_deferred("disabled", true)
+	hitbox.set_deferred("monitorable", false)
+	hitbox.set_deferred("monitoring", false)
+	deadzone.set_deferred("monitorable", false)
+	deadzone.set_deferred("monitoring", false)
+	
+func enable_snow_mole_collision() :
+	snow_mole_collision_enabled = true
+	
+	collision_shape.set_deferred("disabled", false)
+	hitbox.set_deferred("monitorable", true)
+	hitbox.set_deferred("monitoring", true)
+	deadzone.set_deferred("monitorable", true)
+	deadzone.set_deferred("monitoring", true)
+
+func do_hit_stop(duration := 0.5, slowdown_factor := 0.5) -> void:
+	Engine.time_scale = slowdown_factor
+	await get_tree().create_timer(duration * slowdown_factor, true).timeout
+	Engine.time_scale = 1.0
+
+func _on_enemy_dead_zone_class_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Player_StompBox") : 
+		current_state = "DEAD"
+		
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if snow_mole_anim_sprite.animation == "death" : 
+		$snow_mole/DeathParticles.emitting = true
+		snow_mole_anim_sprite.visible = false
+		await get_tree().create_timer(0.6).timeout
+		queue_free()
