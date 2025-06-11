b409c4b47451c15323127dde19d567626cf42f4d
romainmastro <romain.mastro@gmail.com>
2025-06-11 17:17:18 +0200

    update light objects

--- Mouse Journey/Scripts/Environment/treasure_chest_muffler.gd
+++ Mouse Journey/Scripts/Environment/treasure_chest_muffler.gd
@@ -4,5 +4,4 @@ func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerClass :
 		show_opened_chest()
 		GlobalPlayerStats.has_muffler = true
-		GlobalPlayerStats.has_muffler_signal.emit() 
-		# listened by animation component
+		GlobalPlayerStats.has_muffler_signal.emit() # listened by animation component
