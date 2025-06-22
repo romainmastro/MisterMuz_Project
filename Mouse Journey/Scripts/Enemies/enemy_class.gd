a3598dfbdaed400850bf755a52885bff51f92243
romainmastro <romain.mastro@gmail.com>
2025-06-22 21:36:19 +0200

    Enemy : SuperSnowman (1st part) + enemies gives loot

--- Mouse Journey/Scripts/Enemies/enemy_class.gd
+++ Mouse Journey/Scripts/Enemies/enemy_class.gd
@@ -7,6 +7,16 @@ extends CharacterBody2D
 @export var animated_sprite : AnimatedSprite2D
 @export var death_particles : CPUParticles2D
 
+#loot
+@onready var heart_scene = preload("res://Scenes/Collectibles/Collect_Heart.tscn") # 20%
+@onready var fruit_scene = preload("res://Scenes/Collectibles/frost_berry.tscn") # 20%
+@onready var super_fruit_scene = preload("res://Scenes/Collectibles/super_frost_berry.tscn") # 5%
+
+
+var spawn_node : Node
+
+var roll : int = 0
+
 var is_dead : bool = false
 
 func die() : 
@@ -16,7 +26,6 @@ func handle_death():
 	if is_dead : 
 		return
 	is_dead = true
-	print("is dead : ", is_dead)
 	deactivate_collisions()
 	await do_hit_stop()
 
@@ -29,17 +38,25 @@ func handle_death():
 	animated_sprite.play("death")
 	
 func _on_animated_sprite_2d_animation_finished() -> void:
-	print("on animation finished")
 	if animated_sprite.animation == "death":
-		print("FIn de death animation")
 		death_particles.emitting = true
 		animated_sprite.visible = false
 		await get_tree().create_timer(0.6).timeout
+		
+		roll = randi_range(0, 100)
+		if roll >= 1 and roll <= 20 : 
+			spawn_collectible(heart_scene)
+		elif roll >= 21 and roll <= 40 : 
+			spawn_collectible(fruit_scene)
+		elif roll >= 41 and roll <= 45 : 
+			spawn_collectible(super_fruit_scene)
+		else : 
+			pass
+		
 		die()
 
 func _on_dead_zone_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Player_StompBox") : 
-		print("Muz Stomp the spike 2 / on dead zone - enemy class.gd")
 		handle_death()
 
 func deactivate_collisions() : 
@@ -60,3 +77,11 @@ func do_hit_stop(duration := 0.3, slowdown_factor := 0.5) -> void:
 	Engine.time_scale = slowdown_factor
 	await get_tree().create_timer(duration * slowdown_factor, true).timeout
 	Engine.time_scale = 1.0
+
+func spawn_collectible(collectible_scene : PackedScene) : 
+	var collectible = collectible_scene.instantiate() as Area2D
+	collectible.global_position = global_position
+	spawn_node = get_tree().current_scene
+	spawn_node.add_child(collectible)
+
+	
---
+++ Mouse Journey/Scripts/Enemies/super_snowman.gd
@@ -0,0 +1,98 @@
+class_name SuperSnowmanEnemy
+extends enemy_class
+
+@onready var me = preload("res://Scenes/Enemies/super_snowman.tscn")
+
+@export var gravity_component : ClassGravityComponent
+@export var ray : RayCast2D
+@export_enum("gauche", "droite") var direction_départ = "droite"
+@export var speed : float = 30
+@export var damage_amount : float = 1
+@export var ray_offset : int = 10
+var direction : float = 1
+
+var should_flip : bool = false
+
+func _ready() -> void:
+	
+	match direction_départ : 
+		"droite" : 
+			direction = 1
+			animated_sprite.play("walk_right")
+		"gauche" : 
+			direction = -1
+			animated_sprite.play("walk_left")
+			ray.position.x *= -1
+	
+func _physics_process(delta: float) -> void:
+	
+	gravity_component.handle_gravity(self, delta)
+	
+	if not is_dead : 
+		velocity.x = speed * direction
+		move_and_slide()
+		
+		if is_on_wall() : 
+			should_flip = true
+		
+		if not ray.is_colliding() : 
+			should_flip = true
+			
+		if should_flip : 
+			flip_direction()
+		
+		update_animation()
+		
+		update_ray_offset()
+
+func flip_direction() : 
+	direction *= -1
+	ray.position.x *= -1
+	should_flip = false
+
+func update_ray_offset() : 
+	ray.position.x = ray_offset * direction
+
+func get_direction() -> float : 
+	return direction
+
+func update_animation() : 
+	direction = get_direction()
+	
+	if direction == 1 and animated_sprite.animation != "walk_right" : 
+		animated_sprite.play("walk_right")
+		
+	elif direction == -1 and animated_sprite.animation != "walk_left" : 
+			animated_sprite.play("walk_left")
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("traps") : 
+		should_flip = true
+		
+# BASE CLASS OVERRIDE : 
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == "death":
+		death_particles.emitting = true
+		animated_sprite.visible = false
+		await get_tree().create_timer(0.6).timeout
+		
+		roll = randi_range(0, 100)
+		if roll >= 1 and roll <= 20 : 
+			spawn_collectible(heart_scene)
+		elif roll >= 21 and roll <= 40 : 
+			spawn_collectible(fruit_scene)
+		elif roll >= 41 and roll <= 45 : 
+			spawn_collectible(super_fruit_scene)
+		else : 
+			pass
+		
+		# here : spawn 2 little snowmen (angry! quicker?)
+		spawn_little_snowmen()
+		die()
+		
+func spawn_little_snowmen() : 
+	var little_snowman = me.instantiate() as SuperSnowmanEnemy
+	little_snowman.global_position = global_position
+	little_snowman.speed = 60
+	little_snowman.scale = 0.5
