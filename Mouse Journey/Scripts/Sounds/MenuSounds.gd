ac26011335244d0e0aa98b7ec119e0c113d2784a
romainmastro <romain.mastro@gmail.com>
2025-08-04 17:25:36 +0200

    Update Sounds
    1. Sounds for Mouse Nav and Mouse Confirm (Menus)
    2. Redo NavSound (shorten 0.1 sec)

--- Mouse Journey/Scripts/Sounds/MenuSounds.gd
+++ Mouse Journey/Scripts/Sounds/MenuSounds.gd
@@ -3,9 +3,34 @@ extends Node
 @export var nav_sound : AudioStreamPlayer
 @export var conf_sound : AudioStreamPlayer
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_down") or event.is_action_pressed("ui_up") or event.is_action_pressed("ui_left") or event.is_action_pressed("ui_right") : 
+var last_hovered_button : Control = null
+
+func _input(event: InputEvent) -> void:
+	# Keyboard + GamePad
+	if (event.is_action_pressed("ui_down") or 
+		event.is_action_pressed("ui_up") or 
+		event.is_action_pressed("ui_left") or 
+		event.is_action_pressed("ui_right")):
+		
 		if not nav_sound.playing : 
 			nav_sound.play()
+	
+	# Mouse
+	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var hovered = get_viewport().gui_get_hovered_control()
+		if hovered is Button:
+			conf_sound.play()
+			
 	elif event.is_action_pressed("ui_accept") : 
 		conf_sound.play()
+	
+
+func _process(_delta: float) -> void:
+	# Mouse Nav
+	var hovered = get_viewport().gui_get_hovered_control()
+	
+	if hovered != last_hovered_button : 
+		last_hovered_button = hovered
+		if hovered is Button : 
+			nav_sound.play()
+	
